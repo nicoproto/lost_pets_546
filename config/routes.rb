533331eaeefcc07cea4e1f27d9a75441ea0389dd
Nico Proto <nicolasproto@gmail.com>
2021-03-02 18:08:46 +0100

    Added rest of the CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   # As a user I can see all found pets
   get "pets", to: "pets#index"
 
+  # As a user I can add a pet I found
+  get "pets/new", to: "pets#new", as: :new_pet
+  post "pets", to: "pets#create"
+
   # As a user I can see details about one found pet
   get "pets/:id", to: "pets#show", as: :pet
 
-  # As a user I can add a pet I found
-
   # As a user I can update a pet
+  get "pets/:id/edit", to: "pets#edit", as: :edit_pet
+  patch "pets/:id", to: "pets#update"
 
   # As a user I can delete a pet (in a farm)
+  delete "pets/:id", to: "pets#destroy"
 end
